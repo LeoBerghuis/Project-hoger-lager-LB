f45a643a5e01343262da961e6dbe091bffd24e91
LeoBerghuis <leoberghuis@icloud.com>
2024-09-11 14:26:19 +0200

    updates javascipt

--- js/main.js
+++ js/main.js
@@ -1,28 +1,18 @@
 let currentDice = Math.round(Math.random() * 6);
+let currentDiceGuessLower = Math.round(Math.random() * 6);
+let currentDiceGuessHigher = Math.round(Math.random() * 6);
 const dice = document.querySelectorAll('.dice img');
 const diceGuess = document.querySelectorAll('.dice-guess img');
 const totalImages = dice.length;
 const totalImagesGuess = diceGuess.length;
 let intervalId; 
-let startLowerGuess;
-
-const dice1 = document.querySelector('img[alt="Dice 1"]');
-const dice2 = document.querySelector('img[alt="Dice 2"]');
-const dice3 = document.querySelector('img[alt="Dice 3"]');
-const dice4 = document.querySelector('img[alt="Dice 4"]');
-const dice5 = document.querySelector('img[alt="Dice 5"]');
-const dice6 = document.querySelector('img[alt="Dice 6"]');
-const diceGuess1 = document.querySelector('img[alt="Dice Guess 1"]');
-const diceGuess2 = document.querySelector('img[alt="Dice Guess 2"]');
-const diceGuess3 = document.querySelector('img[alt="Dice Guess 3"]');
-const diceGuess4 = document.querySelector('img[alt="Dice Guess 4"]');
-const diceGuess5 = document.querySelector('img[alt="Dice Guess 5"]');
-const diceGuess6 = document.querySelector('img[alt="Dice Guess 6"]');
+let startGuessLow;
+let startGuessHigh
 
 
 document.querySelector('.start-button').addEventListener('click', startCycling);
 document.querySelector('.guess-lower').addEventListener('click', startLower);
-document.querySelector('.guess-higher').addEventListener('click', startLower);
+document.querySelector('.guess-higher').addEventListener('click', startHigher);
 
 function cycleDice() {
      randomDice = Math.round(Math.random() * 6)
@@ -41,21 +31,59 @@ function startCycling() {
     }, 4000); 
 }
 
-function cycleLower() {
-    randomDice = Math.round(Math.random() * 6)
-    diceGuess[currentDice].classList.remove('active-guess');
-    currentDice = (randomDice) % totalImagesGuess;
-    diceGuess[currentDice].classList.add('active-guess');
-    
-}
-
 function startLower() {
 
-    startLowerGuess = setInterval(cycleLower, 100)
+    startGuessLow = setInterval(cycleLower, 100)
 
     setTimeout(function() {
-        clearInterval(startLowerGuess);
+        clearInterval(startGuessLow);
+        guessLower()
     }, 4000);
 }
 
+function cycleLower() {
+    randomDice = Math.round(Math.random() * 6)
+    diceGuess[currentDiceGuessLower].classList.remove('active-guess');
+    currentDiceGuessLower = (randomDice) % totalImagesGuess;
+    diceGuess[currentDiceGuessLower].classList.add('active-guess');
+    
+}
 
+
+function guessLower() {
+    if (currentDiceGuessLower < currentDice) {
+        console.log('You win!');
+    } else if (currentDiceGuessLower === currentDice) {
+        console.log('Tie!');
+    } else {
+        console.log('You lose :(');
+    }
+}
+
+function startHigher() {
+
+    startGuessHigh = setInterval(cycleHigher, 100)
+
+    setTimeout(function() {
+        clearInterval(startGuessHigh);
+        guessHigher()
+    }, 4000);
+}
+
+function cycleHigher() {
+    randomDice = Math.round(Math.random() * 6)
+    diceGuess[currentDiceGuessHigher].classList.remove('active-guess');
+    currentDiceGuessHigher = (randomDice) % totalImagesGuess;
+    diceGuess[currentDiceGuessHigher].classList.add('active-guess');
+    
+}
+
+function guessHigher() {
+    if ( currentDice < currentDiceGuessHigher) {
+        console.log('You win!');
+    } else if (currentDiceGuessHigher === currentDice) {
+        console.log('Tie!');
+    } else {
+        console.log('You lose :(');
+    }
+}
